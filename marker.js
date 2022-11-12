367083ef8973aa0701257a24e8fa5fd1c6df6fad
heejiyang <dianoheeji@naver.com>
2022-11-13 00:55:33 +0900

    add: item

--- marker.js
+++ marker.js
@@ -15,9 +15,3 @@ const marker = [{
         "lat": 33.283767,
         "lng": 126.283465,
 	}];
-
-// fetch(
-//     "https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=10&serviceKey=jcYFh5SvEClxuZ%2FMsh2fRsC%2FPwSAuU%2FeZ4fKPkjtXRcPMhb29kAvZx1ogh84ZLQVLz0dOhwinQTY0uYL88X8Dg%3D%3D"
-// )
-//     .then ((r) => r.json())
-//     .then((r) => console.log(r))
