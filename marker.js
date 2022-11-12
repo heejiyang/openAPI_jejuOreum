535102366f842c9efd6b1a6b9b701cd624b8b168
heejiyang <dianoheeji@naver.com>
2022-11-12 20:15:30 +0900

    add: googlemap and marker.js

---
+++ marker.js
@@ -0,0 +1,23 @@
+const marker = [{
+        "Name": "KIS",
+        "lat": 33.289631,
+        "lng": 126.283982,
+    }, {
+        "Name": "NLCS",
+        "lat": 33.297159,
+        "lng": 126.288322,
+    }, {
+        "Name": "BHA",
+        "lat": 33.291213,
+        "lng": 126.287798,
+	}, {
+        "Name": "SJA",
+        "lat": 33.283767,
+        "lng": 126.283465,
+	}];
+
+// fetch(
+//     "https://api.odcloud.kr/api/15096996/v1/uddi:6738a90c-ec96-4245-a187-9528cea62904?page=1&perPage=10&serviceKey=jcYFh5SvEClxuZ%2FMsh2fRsC%2FPwSAuU%2FeZ4fKPkjtXRcPMhb29kAvZx1ogh84ZLQVLz0dOhwinQTY0uYL88X8Dg%3D%3D"
+// )
+//     .then ((r) => r.json())
+//     .then((r) => console.log(r))
